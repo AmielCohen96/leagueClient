768d05046e64bc4e446f04595d5e7740c501cb72
AmielCohen96 <117925491+AmielCohen96@users.noreply.github.com>
2024-03-11 17:12:29 +0200

    1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import LoginComponent from './components/LoginComponent';
 import SignupComponent from './components/SignupComponent';
+import './App.css'; // Import your CSS file
+
+import backgroundImage from './images/backGround.jpeg';
 
 const App = () => {
     return (
