15b077ecfaf0bbcec27d57a8356281df765fcc51
AmielCohen96 <117925491+AmielCohen96@users.noreply.github.com>
2024-03-18 20:25:43 +0200

    2

---
+++ src/components/streamPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const streamPage = ({ username }) => {
+    return (
+        <div className="welcome-container">
+            <h2>Hello {username}</h2>
+            {/* Add more content for the welcome page */}
+        </div>
+    );
+};
+
+export default streamPage;
